dbecc237136d489236b7de7570b56577518631c8
Andre Powers <andre@stuypend.com>
2021-02-07 15:20:32 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,7 +3,7 @@ def new_hash
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+myhash = { color: "blue"}
 end
 
 def pioneer
@@ -14,3 +14,5 @@ end
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
 end
+
+new_hash
